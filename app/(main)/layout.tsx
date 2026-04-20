3089f0d4f6ef1b575b0eb3acbf49e81bcf5b6eb1
binhnq-seta <binh.nguyen@seta-international.vn>
2026-04-20 20:57:13 +0700

    fix bug
    - remove ignoreDeprecations
    - add icon
    - remove trash

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,24 @@ interface AppLayoutProps {
     children: React.ReactNode;
 }
 
+export const metadata: Metadata = {
+    title: 'Happy Friday',
+    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+    robots: { index: false, follow: false },
+    viewport: { initialScale: 1, width: 'device-width' },
+    openGraph: {
+        type: 'website',
+        title: 'Happy Friday',
+        url: 'https://sakai.primereact.org/',
+        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
+        images: ['https://images.g2crowd.com/uploads/product/image/d737181bd834b948a68e0227ec7c7ada/seta-international.png'],
+        ttl: 604800
+    },
+    icons: {
+        icon: '/layout/images/rice-bowl.png'
+    }
+};
+
 export default function AppLayout({ children }: AppLayoutProps) {
     return <Layout>{children}</Layout>;
 }
